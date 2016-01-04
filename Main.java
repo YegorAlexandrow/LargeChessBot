de9dec80ba77ed4d6257adab214e3d787d5100fc
YegorAlexandrow <yegor99@live.com>
2016-01-04 15:14:34 +0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,10 @@
+
+//Класс, с которого начинается вход в программу
+public class Main {
+	
+	public static void main(String[] args) {
+		new CodeGenerator("").generateOutput();
+		//ChessBoardPointGenerator.generatePoints(30, 5, 60);
+	}
+	
+}
